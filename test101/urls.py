f2dd7519b34ed50e1cce72be0f33638c90cd54c6
Muhammad Abu Bakar <refocuse313@gmail.com>
2020-12-26 16:50:20 +0500

    showing post request data on web page

--- test101/urls.py
+++ test101/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home_view), 
+    path('home/', views.home_view),
+    path('show/',views.show), 
 
 ]
--- test101/views.py
+++ test101/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
   
 # Create your views here. 
 def home_view(request):
-    print(request.POST) 
+    # print(request.POST) 
+    # name=request.POST.get('name')
+
     return render(request, "home.html") 
+
+def show(request):
+    x=request.POST['your_name']
+    print(x)
+    data = {
+        'name': x,
+            }
+    return render(request,"show.html",{'data':data})
